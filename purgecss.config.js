1a9d0f55d365ab79b2289fbf200d9ef3ff44a33e
Yeno13 <robin.berger@hotmail.fr>
2022-06-14 12:58:27 +0200

    Oui

--- purgecss.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    content: ['index.html'],
-    css: ['style.css']
-  }
-  const purgecss = await new PurgeCSS().purge()
-
-  await new PurgeCSS().purge({ 
-    css: ['style.css']
-})
